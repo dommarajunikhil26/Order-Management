1bcb2a8b6891b444f287bb152bea35139929aacf
dommarajunikhil26 <92212838+dommarajunikhil26@users.noreply.github.com>
2024-07-28 01:32:19 -0500

    Added Swagger Documentation

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Order-Management
+Rest Api Swagger documentation available at
+http://localhost:8080/swagger-ui/index.html
--- src/main/java/com/nikhil/order_management/OrderManagementApplication.java
+++ src/main/java/com/nikhil/order_management/OrderManagementApplication.java
@@ -3,6 +3,7 @@ package com.nikhil.order_management;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class OrderManagementApplication {
 
